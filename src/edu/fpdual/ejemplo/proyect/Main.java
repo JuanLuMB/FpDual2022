6bac4a3d7c1f34c2d7c44a61c0f16c07c14e88b3
JuanLuMB <juanluis.s260996@cesurformacion.com>
2022-04-07 10:01:49 +0200

    Segunda subida

---
+++ src/edu/fpdual/ejemplo/proyect/Main.java
@@ -0,0 +1,17 @@
+package edu.fpdual.ejemplo.proyect;
+
+public class Main {
+
+    public static void main(String[] args){
+    new Main().printMenssage();
+    }
+    public void printMenssage(){
+        int variableInt = 5;
+        variableInt = 5;
+        variableInt = 5;
+        variableInt = 5;
+        variableInt = 5;
+        System.out.println(variableInt);
+
+    }
+}
